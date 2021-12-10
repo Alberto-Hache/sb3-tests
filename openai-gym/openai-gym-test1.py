a85d0171276c45bfe8a2e89a5b497764e4f13f98
Alberto H <Alberto@iMac-de-Alberto.local>
2021-12-10 18:17:08 +0100

    Add 2nd tutorial

--- openai-gym/openai-gym-test1.py
+++
@@ -1,8 +0,0 @@
-import gym
-env = gym.make('MsPacman-v0')
-env.reset()
-for t in range(10000):
-    env.render()
-    obs, reward, done, info = env.step(env.action_space.sample()) # take a random action
-    # if done: break
-env.close()
